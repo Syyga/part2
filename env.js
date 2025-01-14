55fb7545df500dc70449f5533f1777d1462e3ebe
Syyga <gsh2672@gmail.com>
2025-01-14 17:49:21 +0900

    Ignore env.js

--- env.js
+++
@@ -1,5 +0,0 @@
-//gsh2672
-//igOPL9a6AAhXDBBy
-
-export const DATABASE_URL =
-  "mongodb+srv://gsh2672:igOPL9a6AAhXDBBy@cluster0.dntwz.mongodb.net/syga?retryWrites=true&w=majority&appName=Cluster0";
